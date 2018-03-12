ab2495bc2c6d753e29cd69fdde80ea8fcab583cb
HunterXHunterT9O <hunterxhuntert9o@gmail.com>
2018-03-13 01:11:20 +0200

    c

--- notifications/consumers.py
+++ notifications/consumers.py
@@ -25,6 +25,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        print('MESSAGE:', message)
 
         # Send message to room group
         await self.channel_layer.group_send(
@@ -38,6 +39,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
+        print('MESSAGE:', message)
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
