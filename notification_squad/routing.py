44c277818e20ef14a6cb0820ef6ba47b2577200d
HunterXHunterT9O <hunterxhuntert9o@gmail.com>
2018-03-12 15:36:24 +0200

    commit

--- notification_squad/routing.py
+++ notification_squad/routing.py
@@ -3,11 +3,13 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import notifications.routing
 
+ASGI_APPLICATION = "notification_squad.routing.application"
+
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
         URLRouter(
-            chat.routing.websocket_urlpatterns
+            notifications.routing.websocket_urlpatterns
         )
     ),
 })
