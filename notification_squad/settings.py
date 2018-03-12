a10853826cf7430dfd166690eb99b4023f2ba02b
HunterXHunterT9O <hunterxhuntert9o@gmail.com>
2018-03-13 01:35:54 +0200

    c

--- notification_squad/settings.py
+++ notification_squad/settings.py
@@ -130,8 +130,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379),
-                     ('notification-squad.herokuapp.com', 6379)],
+            "hosts": [('notification-squad.herokuapp.com', 6379),],
         },
     },
 }
