f3f171401188e92b07c753027d373346995b42de
HunterXHunterT9O <hunterxhuntert9o@gmail.com>
2018-03-13 00:57:50 +0200

    c

--- notifications/views.py
+++ notifications/views.py
@@ -7,6 +7,6 @@ def index(request):
 
 
 def room(request, room_name):
-    return render(request, 'chat/room.html', {
+    return render(request, 'notifications/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
     })
