2af9e7931c6efd5cb29600030dd9e2c4eddb70fc
HunterXHunterT9O <hunterxhuntert9o@gmail.com>
2018-03-16 00:26:22 +0200

    Update consumers.py

--- notifications/consumers.py
+++ notifications/consumers.py
@@ -2,6 +2,7 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 import json
 from django.contrib.auth.models import User
+from . import utils
 
 class NotificationConsumer(WebsocketConsumer):
     
