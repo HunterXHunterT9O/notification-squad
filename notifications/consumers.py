6369d9d6aa429de3cfb61373e6cee44a99fe0eac
HunterXHunterT9O <hunterxhuntert9o@gmail.com>
2018-03-15 20:41:57 +0200

    Update consumers.py

--- notifications/consumers.py
+++ notifications/consumers.py
@@ -40,6 +40,7 @@ class ChatConsumer(WebsocketConsumer):
     # Receive message from room group
     def chat_message(self, event):
         
+        print(event)
         # Send message to WebSocket
         self.send(text_data=json.dumps({
             'username': sender.username
