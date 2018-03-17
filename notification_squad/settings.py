708754620f6fbbecf427f6abd730cc2aa39b642e
HunterXHunterT9O <hunterxhuntert9o@gmail.com>
2018-03-17 06:04:05 +0200

    c

--- notification_squad/settings.py
+++ notification_squad/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ix$d_olgydk6@ok)jdf%uw#!9n&y0$j=)$gf0cnkwld6ftib89'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
